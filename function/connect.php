4eb2859c8472c7fc90669580bf14823a76ff8619
ADRIANSYAH <109329296+drian-o@users.noreply.github.com>
2026-05-28 18:58:20 +0700

    Update connect.php

--- function/connect.php
+++ function/connect.php
@@ -1,21 +1,20 @@
 <?php
-
-/////////////////////////////////////////
-//  LICENSE GAMES BY     : SOFTGAMINGS //
-//  SOURCE CODE BUILD BY : ZULHAYKER   //
-//  COPYRIGHT @2024------ALL RESERVED  //   
-/////////////////////////////////////////
-
 error_reporting(E_ALL ^ E_DEPRECATED);
 
-$host = "mysql-d8b9423-adrnsyah18-eb26.l.aivencloud.com:20528"; //HOST 
-$user = "avnadmin"; //USERNAME 
-$password = "AVNS_Kd07zXk7c_uNqEYPGn9"; //PASSWORD
-$database = "defaultdb"; //DATABASE
+$host = "mysql-d8b9423-adrnsyah18-eb26.l.aivencloud.com"; 
+$user = "avnadmin"; 
+$password = "AVNS_Kd07zXk7c_uNqEYPGn9"; 
+$database = "defaultdb";
+$port = 20528;
 
-$koneksi = mysqli_connect($host,$user,$password,$database) or die(mysqli_error());
-if($koneksi){ 
-}else{ 
-echo "gagal koneksi"; 
+// 1. Inisialisasi MySQLi
+$koneksi = mysqli_init();
+
+// 2. Set bendera SSL (Biar PHP tahu harus konek lewat jalur aman)
+mysqli_ssl_set($koneksi, NULL, NULL, NULL, NULL, NULL);
+
+// 3. Lakukan koneksi dengan menyertakan port dan SSL
+if (!mysqli_real_connect($koneksi, $host, $user, $password, $database, $port, NULL, MYSQLI_CLIENT_SSL)) {
+    die("Koneksi Gagal: " . mysqli_connect_error());
 }
 ?>
